d5344c94f6ac57d7465ef826edca1d3ab28572f3
“rubng87” <gallegoruben_srye@hotmail.com>
2024-04-30 11:56:24 +0200

    ecommercePHP

--- php/ecommerce.php
+++ php/ecommerce.php
@@ -2,11 +2,29 @@
 session_start();
 require_once('connection.php');
 
-var_dump($_SESSION);
+// var_dump($_SESSION);
 
-if (!isset($_SESSION['nombew_cliente'])) {
+if (!isset($_SESSION['nombre_cliente'])) {
     header('Location: ../index.php');
 }
 
-echo "Hola ".$_SESSION['nombre_cliente']. " ".$_SESSION['apellidos_cliente'];
+echo "Hola " . $_SESSION['nombre_cliente'] . " " . $_SESSION['apellidos_cliente'];
 
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="logout.php">Cerrar sesión</a>
+
+
+</body>
+
+</html>
