6b5fda96d41133e2f0f99c715e4e6a9bd5063da7
“rubng87” <gallegoruben_srye@hotmail.com>
2024-04-30 13:18:17 +0200

    ecommercePHP

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
 <html lang="es">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>ECommerce</title>
-  <link rel="stylesheet" href="css/style.css" />
-</head>
-
-<body>
-  <header>
-    <h1>Tutti Frutti</h1>
-  </header>
-  <main>
-    <section id="signin">
-      <fieldset>
-        <legend>Formulario de acceso</legend>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ECommerce</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Tutti Frutti</h1>
+    </header>
+    <main>
+      <section id="signin">
+        <h2>Formulario de acceso</h2>
         <form action="php/signin.php" method="post">
           <div>
             <label for="email">Email: </label>
@@ -30,71 +27,71 @@
             <button type="reset">Borrar</button>
             <button type="button" onclick="mostrarFormularioAlta()">Nuevo usuario</button>
           </div>
-      </fieldset>
-      </form>
-    </section>
+        </form>
+      </section>
 
-    <section id="signup">
-      <h2>Por favor, introduzca sus datos</h2>
-      <form action="php/signup.php" method="post">
-        <fieldset>
-          <legend>Datos personales</legend>
-          <div>
-            <label for="nombre">Nombre:</label>
-            <input type="text" name="nombre" id="nombre" />
-          </div>
+      <section id="signup">
+        <h2>Por favor, introduzca sus datos</h2>
+        <!-- <form id="signupForm" action="php/signup.php" method="post"> -->
+        <form id="signupForm">
+          <fieldset>
+            <legend>Datos personales</legend>
+            <div>
+              <label for="nombre">Nombre:</label>
+              <input type="text" name="nombre" id="nombre"  min="2" max="20"/>
+            </div>
 
-          <div>
-            <label for="apellidos">Apellidos:</label>
-            <input type="text" name="apellidos" id="apellidos" />
-          </div>
+            <div> 
+              <label for="apellidos">Apellidos:</label>
+              <input type="text" name="apellidos" id="apellidos"  min="2" max="50"/>
+            </div>
 
-          <div>
-            <label for="password1">Contraseña elegida:</label>
-            <input type="text" name="password1" id="password1" />
-          </div>
+            <div>
+              <label for="password1">Contraseña elegida:</label>
+              <input type="password" name="password1" id="password1"  min="8" max="20"/>
+            </div>
 
-          <div>
-            <label for="password2">Confirme la contraseña:</label>
-            <input type="text" name="password2" id="password2" />
-          </div>
-          <div>
-            <label for="email">Email:</label>
-            <input type="text" name="email" id="email" />
-          </div>
-        </fieldset>
+            <div>
+              <label for="password2">Confirme la contraseña:</label>
+              <input type="password" name="password2" id="password2"  min="8" max="20"/>
+            </div>
+            <div id="errorPassword"></div>
+            <div>
+              <label for="emailUp">Email:</label>
+              <input type="email" name="email" id="email" />
+            </div>
+          </fieldset>
 
-        <fieldset>
-          <legend>Datos de la compra</legend>
-          <div>
-            <label for="nif">NIF:</label>
-            <input type="text" name="nif" id="nif" />
-          </div>
+          <fieldset>
+            <legend>Datos de la compra</legend>
+            <div>
+              <label for="nif">NIF:</label>
+              <input type="text" name="nif" id="nif" min="9" max="9" />
+            </div>
 
-          <div>
-            <label for="telefono">Teléfono</label>
-            <input type="text" name="telefono" id="telefono" />
-          </div>
+            <div>
+              <label for="telefono">Teléfono</label>
+              <input type="text" name="telefono" id="telefono" min="9" max="9" />
+            </div>
 
-          <div>
-            <label for="direccion">Dirección:</label>
-            <input type="text" name="direccion" id="direccion" />
-          </div>
+            <div>
+              <label for="direccion">Dirección:</label>
+              <input type="text" name="direccion" id="direccion"  />
+            </div>
 
+            <div>
+              <label for="ciudad">Ciudad</label>
+              <input type="text" name="ciudad" id="ciudad" min="2" max="20"/>
+            </div>
+          </fieldset>
           <div>
-            <label for="ciudad">Ciudad</label>
-            <input type="text" name="ciudad" id="ciudad" />
+            <button type="submit">Enviar datos</button>
+            <button type="reset">Borrar datos</button>
+            <button type="button" onclick="volverInicio()">Cancelar</button>
           </div>
-        </fieldset>
-        <div>
-          <button type="submit">Enviar datos</button>
-          <button type="reset">Borrar datos</button>
-          <button type="button" onclick="volverInicio()">Cancelar</button>
-        </div>
-      </form>
-    </section>
-  </main>
-  <script src="js/app.js"></script>
-</body>
-
+        </form>
+      </section>
+    </main>
+    <script src="js/app.js"></script>
+  </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-
-
-
 // Ocultar formulario de alta 
 document.getElementById("signup").style.display = "none";
 
@@ -14,3 +11,40 @@ function volverInicio() {
     document.getElementById("signin").style.display = "block";
     document.getElementById("signup").style.display = "none";
 }
+
+
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    let nombre = document.getElementById("nombre").value.trim().toLower;
+    console.log(nombre);
+});
+//     let password1 = document.getElementById("password1").value;
+//     let password2 = document.getElementById("password2").value;
+
+//     if (password1!== password2) {
+//         document.getElementById("errorPassword").innerHTML = "<p>Las contraseñas no coinciden</p>";
+//         document.getElementById("password2").value = ""
+//         return;
+//     } else {
+//         document.getElementById("errorPassword").innerHTML = "<p>Las contraseñas coinciden</p>";
+//         return;
+//     }
+
+//     signupForm.reset();
+// });
+
+function corregirNombre(nombre) {
+
+    let nombreCorregido = "";
+    let temp = nombre.split(" ");
+
+    // de del el la los las
+   
+   
+    return nombreCorregido
+}
+
