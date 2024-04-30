a65e75a7f27ece3cc044174d9b83f45fcd38f1c7
“rubng87” <gallegoruben_srye@hotmail.com>
2024-04-30 10:50:15 +0200

    ecomercePHP

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>ECommerce</title>
-    <link rel="stylesheet" href="css/style.css" />
-  </head>
-  <body>
-    <header>
-      <h1>Tutti Frutti</h1>
-    </header>
-    <main>
-      <section id="signin">
-        <h2>Formulario de acceso</h2>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>ECommerce</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+  <header>
+    <h1>Tutti Frutti</h1>
+  </header>
+  <main>
+    <section id="signin">
+      <fieldset>
+        <legend>Formulario de acceso</legend>
         <form action="php/signin.php" method="post">
           <div>
             <label for="email">Email: </label>
@@ -27,69 +30,71 @@
             <button type="reset">Borrar</button>
             <button type="button" onclick="mostrarFormularioAlta()">Nuevo usuario</button>
           </div>
-        </form>
-      </section>
+      </fieldset>
+      </form>
+    </section>
 
-      <section id="signup">
-        <h2>Por favor, introduzca sus datos</h2>
-        <form action="php/signup.php" method="post">
-          <fieldset>
-            <legend>Datos personales</legend>
-            <div>
-              <label for="nombre">Nombre:</label>
-              <input type="text" name="nombre" id="nombre" />
-            </div>
-
-            <div>
-              <label for="apellidos">Apellidos:</label>
-              <input type="text" name="apellidos" id="apellidos" />
-            </div>
-
-            <div>
-              <label for="password1">Contraseña elegida:</label>
-              <input type="text" name="password1" id="password1" />
-            </div>
-
-            <div>
-              <label for="password2">Confirme la contraseña:</label>
-              <input type="text" name="password2" id="password2" />
-            </div>
-            <div>
-              <label for="email">Email:</label>
-              <input type="text" name="email" id="email" />
-            </div>
-          </fieldset>
-
-          <fieldset>
-            <legend>Datos de la compra</legend>
-            <div>
-              <label for="nif">NIF:</label>
-              <input type="text" name="nif" id="nif" />
-            </div>
-
-            <div>
-              <label for="telefono">Teléfono</label>
-              <input type="text" name="telefono" id="telefono" />
-            </div>
-
-            <div>
-              <label for="direccion">Dirección:</label>
-              <input type="text" name="direccion" id="direccion" />
-            </div>
-
-            <div>
-              <label for="ciudad">Ciudad</label>
-              <input type="text" name="ciudad" id="ciudad" />
-            </div>
-          </fieldset>
+    <section id="signup">
+      <h2>Por favor, introduzca sus datos</h2>
+      <form action="php/signup.php" method="post">
+        <fieldset>
+          <legend>Datos personales</legend>
           <div>
-            <button type="submit">Enviar datos</button>
-            <button type="reset">Borrar datos</button>
-            <button type="button" onclick="volverInicio()">Cancelar</button>
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre" />
           </div>
-        </form>
-      </section>
-    </main>
-    <script src="js/app.js"></script>
-  </body>
+
+          <div>
+            <label for="apellidos">Apellidos:</label>
+            <input type="text" name="apellidos" id="apellidos" />
+          </div>
+
+          <div>
+            <label for="password1">Contraseña elegida:</label>
+            <input type="text" name="password1" id="password1" />
+          </div>
+
+          <div>
+            <label for="password2">Confirme la contraseña:</label>
+            <input type="text" name="password2" id="password2" />
+          </div>
+          <div>
+            <label for="email">Email:</label>
+            <input type="text" name="email" id="email" />
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Datos de la compra</legend>
+          <div>
+            <label for="nif">NIF:</label>
+            <input type="text" name="nif" id="nif" />
+          </div>
+
+          <div>
+            <label for="telefono">Teléfono</label>
+            <input type="text" name="telefono" id="telefono" />
+          </div>
+
+          <div>
+            <label for="direccion">Dirección:</label>
+            <input type="text" name="direccion" id="direccion" />
+          </div>
+
+          <div>
+            <label for="ciudad">Ciudad</label>
+            <input type="text" name="ciudad" id="ciudad" />
+          </div>
+        </fieldset>
+        <div>
+          <button type="submit">Enviar datos</button>
+          <button type="reset">Borrar datos</button>
+          <button type="button" onclick="volverInicio()">Cancelar</button>
+        </div>
+      </form>
+    </section>
+  </main>
+  <script src="js/app.js"></script>
+</body>
+
 </html>
--- php/signin.php
+++ php/signin.php
@@ -1,25 +1,31 @@
 <?php
+session_start();
 
 require_once('connection.php');
 
 $email = $_POST['email'];
 $password_user = $_POST['password'];
 
-$select = "SELECT password FROM clientes WHERE email = ?";
+$select = "SELECT * FROM clientes WHERE email = ?";
 $query = $conn->prepare($select);
 $query->execute([$email]);
 $result = $query->fetch();
 
 // var_dump($result);
 
+// Por si el login no es correcto
 if (!$result) {
     echo "Usuario o contraseña incorrectos";
-    die();
+    header('Location: index.php');
+    // die();
 }
 
+// Login ha sido correcto y obtenemos los datos del cliente
 $password_hash = $result['password'];
 if (password_verify($password_user, $password_hash)) {
-    echo "Usuario o contraseña correctos";
+    // echo "Usuario o contraseña correctos";
+    $_SESSION['nombre_cliente'] = $result['nombre_cliente'];
+    $_SESSION['apellidos_cliente'] = $result['apellidos_cliente'];
     header('Location: ecommerce.php');
     
 } else {
